78195c37b7b6f1b2648a30272fb6914c6162f2cc
Yvaniak <pro@yvaniak.fr>
2025-02-18 11:51:58 +0100

    fix(nix): flake checker hooks if flake enabled

--- nix.nix
+++ nix.nix
@@ -13,12 +13,16 @@
   config = lib.mkIf config.nix.enable {
     languages.nix.enable = true;
 
-    git-hooks.hooks = {
-      nixfmt-rfc-style.enable = true;
-      statix.enable = true;
-      deadnix.enable = true;
-      commitizen.enable = true;
-    };
+    git-hooks.hooks =
+      {
+        nixfmt-rfc-style.enable = true;
+        statix.enable = true;
+        deadnix.enable = true;
+        commitizen.enable = true;
+      }
+      // lib.attrsets.optionalAttrs config.nix.flake.enable {
+        flake-checker.enable = true;
+      };
 
     common.tests = lib.mkIf config.nix.flake.enable ''nix build'';
 
