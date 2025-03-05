842f6773bdfef65750ffaba65b4d214027d6ae60
Yvaniak <pro@yvaniak.fr>
2025-03-05 21:48:25 +0100

    fix(c): added meson option with just build an test and meson and ninja packages

--- c.nix
+++ c.nix
@@ -6,14 +6,26 @@
 }:
 {
   options = {
-    mydevenvs.c.enable = lib.mkEnableOption "enable c devenv";
+    mydevenvs.c = {
+      enable = lib.mkEnableOption "enable c devenv";
+      meson.enable = lib.mkEnableOption "enable option in just";
+    };
   };
 
   config = lib.mkIf config.mydevenvs.c.enable {
-    packages = lib.mkIf config.mydevenvs.global.packages.enable [ pkgs.clang ];
+    packages = lib.mkIf config.mydevenvs.global.packages.enable [
+      pkgs.clang
+      (lib.mkIf config.mydevenvs.c.meson.enable pkgs.meson)
+      (lib.mkIf config.mydevenvs.c.meson.enable pkgs.ninja)
+    ];
 
     git-hooks.hooks = lib.mkIf config.mydevenvs.global.hooks.enable {
       clang-format.enable = true;
+    };
+
+    mydevenvs.tools.just = lib.mkIf config.mydevenvs.c.meson.enable {
+      just-test = lib.mkIf config.mydevenvs.global.enterTest.enable "  meson test -C builddir";
+      just-build = lib.mkIf config.mydevenvs.global.scripts.enable "  meson compile -C builddir";
     };
   };
 }
